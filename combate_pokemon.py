20ee07153390d48c26d4eb7da76ccfc527038add
SimRose <nikko_6_6_6@hotmail.it>
2018-01-19 02:48:44 -0300

    Modificado el combate_pokemon. Ahora los pokemones poseen dos ataques que usan de manera random Bola voltio puede paralizar al pokemon enemigo bola voltio ahora hace 9 de daño (antes 12)

--- combate_pokemon.py
+++ combate_pokemon.py
@@ -9,29 +9,44 @@ chance_voltio = 8
 vida_enemigo = 0
 ataque_pokemon = 0
 chance_enemigo = 0
+ataque_1 = 0
+ataque_2 = 0
+nombre_ataque_1 = ""
+nombre_ataque_2 = ""
+
 
 if pokemon_elegido == "SQUIRTLE":
     vida_enemigo = 90
     nombre_pokemon = "Squirtle"
-    ataque_pokemon = 8
+    ataque_1 = 10
+    ataque_2 = 9
+    nombre_ataque_1 = "Burbuja"
+    nombre_ataque_2 = "Coletazo"
     chance_enemigo = 9
+
 
 elif pokemon_elegido == "CHARMANDER":
     vida_enemigo = 80
     nombre_pokemon = "Charmander"
-    ataque_pokemon = 10
     chance_enemigo = 9
+    ataque_1 = 11
+    ataque_2 = 10
+    nombre_ataque_1 = "Llamarada"
+    nombre_ataque_2 = "Garra"
 
 elif pokemon_elegido == "BULBASAUR":
     vida_enemigo = 100
     nombre_pokemon = "Bulbasaur"
-    ataque_pokemon = 7
+    ataque_1 = 9
+    ataque_2 = 8
+    nombre_ataque_1 = "Latigo"
+    nombre_ataque_2 = "Ataque rapido"
     chance_enemigo = 9
 
 while vida_pikachu > 0 and vida_enemigo > 0:
 
-    ataque_elegido = input("¿Qué ataque vamos a usar? (Chispazo / Bola voltio)").upper()
-    while ataque_elegido != "CHISPAZO" and ataque_elegido != "BOLA VOLTIO":
+    ataque_elegido = ""
+    while not (ataque_elegido == "CHISPAZO" or ataque_elegido == "BOLA VOLTIO"):
         ataque_elegido = input("¿Qué ataque vamos a usar? (Chispazo / Bola voltio)").upper()
 
     import random
@@ -45,7 +60,7 @@ while vida_pikachu > 0 and vida_enemigo > 0:
     elif ataque_elegido == "BOLA VOLTIO":
         if chance_ataque <= chance_voltio:
             print("¡Has utilizado Bola voltio!")
-            vida_enemigo -= 12
+            vida_enemigo -= 9
         else:
             print("¡Tu ataque ha fallado! ")
 
@@ -55,12 +70,24 @@ while vida_pikachu > 0 and vida_enemigo > 0:
         print("Has derrotado al {} enemigo".format(nombre_pokemon))
 
     if vida_enemigo > 0:
-        ataque_enemigo = random.randint(1, 10)
-        if ataque_enemigo <= chance_enemigo:
-            print("{} te hace un ataque de {} de daño".format(nombre_pokemon, ataque_pokemon))
-            vida_pikachu -= ataque_pokemon
-        else:
-            print("¡El ataque enemigo ha fallado!")
+        if ataque_elegido == "BOLA VOLTIO" and chance_ataque <= chance_voltio:
+            paralizar = random.randint(1, 10)
+            if paralizar > 8:
+                print("¡El pokemon enemigo está paralizado y no puede atacar!")
+            else:
+                numero_ataque = random.randint(1, 10)
+                if numero_ataque > 5:
+                    ataque_enemigo = ataque_1
+                    nombre_ataque = nombre_ataque_1
+                else:
+                    ataque_enemigo = ataque_2
+                    nombre_ataque = nombre_ataque_2
+                chance_ataque_enemigo = random.randint(1, 10)
+                if chance_ataque_enemigo <= chance_enemigo:
+                    print("El {} enemigo usa {} y te hace {} de daño".format(nombre_pokemon, nombre_ataque, ataque_enemigo))
+                    vida_pikachu -= ataque_enemigo
+                else:
+                    print("¡El ataque enemigo ha fallado!")
     print("La vida de Pikachu es de {}".format(vida_pikachu))
 
 if vida_enemigo <= 0:
